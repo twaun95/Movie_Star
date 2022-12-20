e24e0c68a46cc54b0a857e2d8b0dc3fb894389ec
taewaun <taewaunkwon@gmail.com>
2022-12-20 23:17:10 +0900

    feat : Application 에 Timber 라이브러리 추가

--- app/src/main/java/com/twaun95/moviestar/application/MovieStarApplication.kt
+++ app/src/main/java/com/twaun95/moviestar/application/MovieStarApplication.kt
@@ -2,10 +2,16 @@ package com.twaun95.moviestar.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MovieStarApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initLogger()
+    }
+
+    private fun initLogger() {
+        Timber.plant(Timber.DebugTree())
     }
 }
