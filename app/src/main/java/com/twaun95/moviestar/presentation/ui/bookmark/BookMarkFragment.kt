b13f5027cd22cb6475572ff0afffc2354c079973
taewaun <taewaunkwon@gmail.com>
2022-12-24 17:32:52 +0900

    feat : 라이프사이클 로그 확인

--- app/src/main/java/com/twaun95/moviestar/presentation/ui/bookmark/BookMarkFragment.kt
+++ app/src/main/java/com/twaun95/moviestar/presentation/ui/bookmark/BookMarkFragment.kt
@@ -1,7 +1,12 @@
 package com.twaun95.moviestar.presentation.ui.bookmark
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.twaun95.moviestar.R
+import com.twaun95.moviestar.application.Logger
 import com.twaun95.moviestar.databinding.FragmentBookmarkBinding
 import com.twaun95.moviestar.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +15,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class BookMarkFragment : BaseFragment<FragmentBookmarkBinding, BookMarkFragmentViewModel>(R.layout.fragment_bookmark) {
     override val fragmentVM by viewModels<BookMarkFragmentViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Logger.d("BookMarkFragment onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Logger.d("BookMarkFragment onViewCreated")
+    }
     companion object {
         fun getInstance() : BookMarkFragment = BookMarkFragment()
     }
--- app/src/main/java/com/twaun95/moviestar/presentation/ui/search/SearchFragment.kt
+++ app/src/main/java/com/twaun95/moviestar/presentation/ui/search/SearchFragment.kt
@@ -1,7 +1,12 @@
 package com.twaun95.moviestar.presentation.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.twaun95.moviestar.R
+import com.twaun95.moviestar.application.Logger
 import com.twaun95.moviestar.databinding.FragmentSearchBinding
 import com.twaun95.moviestar.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +15,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchFragmentViewModel>(R.layout.fragment_search){
     override val fragmentVM by viewModels<SearchFragmentViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Logger.d("SearchFragment onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Logger.d("SearchFragment onViewCreated")
+    }
     companion object {
         fun getInstance() : SearchFragment = SearchFragment()
     }
