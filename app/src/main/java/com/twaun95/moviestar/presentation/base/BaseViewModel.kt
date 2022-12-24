32a6e3f0a780b07aa00f248f023aaaa6e0a701e5
taewaun <taewaunkwon@gmail.com>
2022-12-24 18:54:23 +0900

    feat : BaseViewModel 에 공통 로딩 변수 작성

--- app/src/main/java/com/twaun95/moviestar/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/twaun95/moviestar/presentation/base/BaseViewModel.kt
@@ -1,7 +1,17 @@
 package com.twaun95.moviestar.presentation.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 
 abstract class BaseViewModel : ViewModel() {
 
+    protected val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean>
+        get() = _loading
+
+    protected fun startLoading() { _loading.value = true }
+    protected fun stopLoading() { _loading.value = false }
 }
