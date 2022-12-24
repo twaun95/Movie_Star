c914a855d6cf0e64ff9f680ae4fa684b800dd2b1
taewaun <taewaunkwon@gmail.com>
2022-12-24 16:47:30 +0900

    feat : minSdkVersion 20 을 위한 multidex 추가

--- app/src/main/java/com/twaun95/moviestar/application/MovieStarApplication.kt
+++ app/src/main/java/com/twaun95/moviestar/application/MovieStarApplication.kt
@@ -1,11 +1,12 @@
 package com.twaun95.moviestar.application
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class MovieStarApplication : Application() {
+class MovieStarApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         initLogger()
